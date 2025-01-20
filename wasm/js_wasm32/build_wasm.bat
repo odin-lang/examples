1d62065ba67dc579b81f8f8460331bccb1bce583
Karl Zylinski <karl@zylinski.se>
2025-01-20 17:57:55 +0100

    Fixed wasm example windows build

--- wasm/js_wasm32/build_wasm.bat
+++
@@ -1,4 +0,0 @@
-for /f "delims=" %%i in ('odin.exe root') do set "ODIN_ROOT=%%i"
-copy "%ODIN_ROOT%\core\sys\wasm\js\odin.js" "web\odin.js"
-
-call odin.exe build . -target:js_wasm32 -out:web\index.wasm
